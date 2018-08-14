57c851c27e64d057a31b15a1b8397ae031c7ddbf
helloaija <shitongle163@126.com>
2018-08-14 16:22:03 +0800

    spring-cloud

--- eureka-client/src/main/java/com/stl/cloud/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/stl/cloud/eurekaclient/EurekaClientApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
+        System.out.println();
         SpringApplication.run(EurekaClientApplication.class, args);
     }
 }
