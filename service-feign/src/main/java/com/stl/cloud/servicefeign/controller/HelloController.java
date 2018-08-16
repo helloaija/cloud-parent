3679a917f52860182bf2859f69d96be0aa6e81fc
helloaija <shitongle163@126.com>
2018-08-16 17:19:56 +0800

    config-server

--- service-feign/src/main/java/com/stl/cloud/servicefeign/controller/HelloController.java
+++ service-feign/src/main/java/com/stl/cloud/servicefeign/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.stl.cloud.servicefeign.controller;
 
 import com.stl.cloud.servicefeign.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +13,15 @@ public class HelloController {
     @Autowired
     private HelloService helloService;
 
+    @Value("${configTest}")
+    private String configTest;
+
     @RequestMapping(value="/helloPort")
     public String helloPort() {
         return helloService.helloPort();
+    }
+
+    public String helloConfig() {
+        return configTest;
     }
 }
