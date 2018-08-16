003e212ea160cb958b9c509f708d354fecdbe520
helloaija <shitongle163@126.com>
2018-08-16 10:51:51 +0800

    hystrix-dashboard

--- service-rebbon/src/main/java/com/stl/cloub/servicerebbon/ServiceRebbonApplication.java
+++ service-rebbon/src/main/java/com/stl/cloub/servicerebbon/ServiceRebbonApplication.java
@@ -1,14 +1,18 @@
 package com.stl.cloub.servicerebbon;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableHystrix
+@EnableHystrixDashboard
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceRebbonApplication {
@@ -21,5 +25,15 @@ public class ServiceRebbonApplication {
     @LoadBalanced
     RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public ServletRegistrationBean<HystrixMetricsStreamServlet> getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<>(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
