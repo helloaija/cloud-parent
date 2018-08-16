639a2586b6d4f219d6cd5fd27848b6ee4f6927e1
helloaija <shitongle163@126.com>
2018-08-16 09:15:21 +0800

    hystrix

--- service-feign/src/main/java/com/stl/cloud/servicefeign/service/HelloService.java
+++ service-feign/src/main/java/com/stl/cloud/servicefeign/service/HelloService.java
@@ -1,9 +1,10 @@
 package com.stl.cloud.servicefeign.service;
 
+import com.stl.cloud.servicefeign.service.impl.HelloServiceImpl;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(value="eurekaClient")
+@FeignClient(value="eurekaClient", fallback = HelloServiceImpl.class)
 public interface HelloService {
 
     @RequestMapping(value="/hello/helloPort")
