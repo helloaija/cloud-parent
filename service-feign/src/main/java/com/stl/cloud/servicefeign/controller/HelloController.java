efa47b3ad78f590981f4a964fd230c7be19400bd
helloaija <shitongle163@126.com>
2018-08-20 13:51:11 +0800

    spring cloud bus

--- service-feign/src/main/java/com/stl/cloud/servicefeign/controller/HelloController.java
+++ service-feign/src/main/java/com/stl/cloud/servicefeign/controller/HelloController.java
@@ -3,9 +3,11 @@ package com.stl.cloud.servicefeign.controller;
 import com.stl.cloud.servicefeign.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/hello")
 public class HelloController {
